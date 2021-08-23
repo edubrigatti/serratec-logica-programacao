1aa0c243592fd1b1f0a752a2e64ceee1573d78b1
Edubrigatti <edupina@brigatti.com>
2021-08-23 17:43:20 -0300

    subir exercicio

---
+++ fase3/temperatura.js
@@ -0,0 +1,15 @@
+/*O programa “termômetro” lê uma temperatura em graus Celsius, e devolve sua equivalência na escala Fahrenheit*/
+
+var prompt = require('prompt-sync')();
+
+ var temperatura = prompt ("Quanto está sua temperatura?");
+ var Fahrenheit = temperatura * 1.8 + 32;45
+ 
+ 
+
+ console.log("A sua temperatura em Fahrenheit é: " + Fahrenheit);
+
+
+
+
+ /*var Fahrenheit = temperatura * 1.8 + 32;*/
